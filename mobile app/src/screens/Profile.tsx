ee594497443cdcfbfc0fdb3ca551262029f665c9
Harshal Dave <harshaldave74@gmail.com>
2023-01-29 13:57:34 +0530

    :sparkles: feat: profile done

---
+++ mobile app/src/screens/Profile.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import SafeArea from '../components/SafeArea'
+
+const Profile = () => {
+  return (
+    <SafeArea>
+      <Text>Profile</Text>
+    </SafeArea>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
