b3723872cbbfdf0181c9bd797d52e0236b14ce71
Harshal Dave <harshaldave74@gmail.com>
2023-01-29 13:58:46 +0530

    :bug: fix: ai integration, employee management

---
+++ mobile app/src/screens/AiPrediction.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AiPrediction = () => {
+  return (
+    <View>
+      <Text>AiPrediction</Text>
+    </View>
+  )
+}
+
+export default AiPrediction
+
+const styles = StyleSheet.create({})
---
+++ mobile app/src/screens/EmployeeManage.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import SafeArea from '../components/SafeArea'
+
+const EmployeeManage = () => {
+  return (
+    <SafeArea>
+      <Text>EmployeeManage</Text>
+    </SafeArea>
+  )
+}
+
+export default EmployeeManage
+
+const styles = StyleSheet.create({})
