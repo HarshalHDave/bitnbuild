6271b30e3eac474da062d2b33e035aca89111211
Harshal Dave <harshaldave74@gmail.com>
2023-01-29 13:57:17 +0530

    :sparkles: feat: 3rd party api micro app done

--- mobile app/src/screens/Orders.tsx
+++ mobile app/src/screens/Orders.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import SafeArea from '../components/SafeArea'
 import OrderCard from '../components/OrderCard'
@@ -15,6 +15,11 @@ const Orders = () => {
           <View style={{ margin: 20, padding: 15, marginTop: 30, borderRadius: 20, justifyContent: 'center', alignItems: "center", height: "auto", elevation: 7 }}>
             <Text style={{textAlign: 'center', fontSize: 14, color: '#ded0de'}} selectable={true}>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJiYXJmaSIsImlhdCI6MTY3NDkxMjAxOCwiZXhwIjoxNzA2NDQ4MDE4fQ.U5Xtdu7E5SIu4XWnaDXiF0kdF2QdK4FBbRguqzq3bbM</Text>
           </View>
+          <TouchableOpacity 
+          style={{padding: 16, width: '100%', borderRadius: 8, borderWidth: 0.8, borderColor: "#cec0ce", alignItems: 'center', justifyContent: 'center'}}
+            onPress={() => { Linking.openURL("http://adhesive-rat.surge.sh/postman_documentation.html#admin-product")}}>
+            <Text style={{ color: "#48EFEF", fontSize: 16, textAlign: 'center', fontWeight: '600' }}>See Documentation</Text>
+          </TouchableOpacity>
           <Text style={{color: "#beb0be", fontSize: 14, position: 'absolute', bottom: 16}}>Share this token with the parties you trust. They will have the acces to view and alter the data in your invneotry.</Text>
         </View>
       </>
